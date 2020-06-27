e7943a8fd4d0f8bacbdc031dd4f7ee0fb3bb544b
abi11 <abhiyendra5@yahoo.com>
2020-06-26 21:52:04 -0400

    base commit for existing project

---
+++ src/test/java/com/abhi/microservices/ratingsdateservice/RatingsDateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.microservices.ratingsdateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
